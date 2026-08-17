f27751b31aed2eb800137bf625920836d7692ecb
zi.ziaee <ziaee.dev@gmail.com>
2026-08-17 19:28:58 +0330

    changed code base in text-field component

--- resources/views/components/form/text-field.blade.php
+++ resources/views/components/form/text-field.blade.php
@@ -1,11 +1,12 @@
 <div class="form-group row">
-  <input 
-      id="{{ $name }}"
-      type="{{ $type }}"
-      name="{{ $name }}"
-      placeholder="{{ $placeholder ?? '' }}"
-      value="{{ old($name) ?: ($object->{$name} ?? '') }}"
-      {{ $attributes->merge(['class' => 'form-control ' . ($class ?? '')]) }}
-  >
+    <input
+        id="{{ $id ?: $name }}"
+        type="{{ $type }}"
+        name="{{ $name }}"
+        placeholder="{{ $placeholder ?? '' }}"
+        value="{{ old($name) ?: ($object->{$name} ?? '') }}"
+        {{ $attributes->merge([
+            'class' => 'form-control ' . ($class ?? '')
+        ]) }}
+    >
 </div>
-
