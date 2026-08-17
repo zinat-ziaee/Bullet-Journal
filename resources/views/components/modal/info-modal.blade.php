6cbdc2692980d90270fec02b27a2d15630d1059e
zi.ziaee <ziaee.dev@gmail.com>
2026-08-17 19:29:49 +0330

    changed code base in info-modal component

--- resources/views/components/modal/info-modal.blade.php
+++ resources/views/components/modal/info-modal.blade.php
@@ -7,15 +7,16 @@
       </div>
       <div id="tabs" class="modal-body">
 
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
+
+        <ul class="nav nav-tabs mb-3" id="myTab" role="tablist">
           <li class="nav-item" role="presentation">
-            <button class="nav-link" id="note-tab" data-bs-toggle="tab" data-bs-target="#note" type="button" role="tab" aria-controls="note" aria-selected="true">یادداشت</button>
+            <button class="nav-link" id="note-tab" data-bs-toggle="tab" data-bs-target="#note" type="button" role="tab">یادداشت</button>
           </li>
           <li class="nav-item" role="presentation">
-            <button class="nav-link" id="event-tab" data-bs-toggle="tab" data-bs-target="#event" type="button" role="tab" aria-controls="event" aria-selected="false">رویداد</button>
+            <button class="nav-link" id="event-tab" data-bs-toggle="tab" data-bs-target="#event" type="button" role="tab">رویداد</button>
           </li>
           <li class="nav-item" role="presentation">
-            <button class="nav-link" id="task-tab" data-bs-toggle="tab" data-bs-target="#task" type="button" role="tab" aria-controls="task" aria-selected="true">تسک</button>
+            <button class="nav-link" id="task-tab" data-bs-toggle="tab" data-bs-target="#task" type="button" role="tab">تسک</button>
           </li>
         </ul>
        
@@ -24,7 +25,12 @@
             <x-form.body class="formNode">
               <x-form.text-field type="hidden" name="note_id" id="note_id"/>
               <x-form.text-field name="title" required/>
-              <x-form.textarea name="description" class="description" id="description1" rows="4" cols="52" required/>
+              <x-form.textarea name="description" class="form-control w-100 description" id="description1" rows="4" cols="52" required/>
+              <x-form.text-field  data-jdp data-jdp-only-date name="log_date" id="note_log_date" class="datetimepicker" required>
+                <span class="input-group-addon">
+                  <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+              </x-form.text-field>
               <x-form.submit id="saveNode"/>
             </x-form.body> 
           </div>
@@ -49,7 +55,12 @@
             <x-form.body class="formTask">
               <x-form.text-field type="hidden" name="task_id" id="task_id"/>
               <x-form.text-field name="title" required/>
-              <x-form.textarea name="description" class="description" id="description2" rows="4" cols="57" required/>
+              <x-form.textarea name="description" class="form-control w-100 description" id="description2" rows="4" cols="57" required/>
+              <x-form.text-field  data-jdp data-jdp-only-date name="log_date" id="task_log_date" class="datetimepicker" required>
+                <span class="input-group-addon">
+                  <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+              </x-form.text-field>
               <x-form.submit id="saveTask"/>
             </x-form.body>
           </div>
