147b6e06e0b31fbffe9b594a48bae577c5094524
zi.ziaee <ziaee.dev@gmail.com>
2026-09-24 12:15:30 +0330

    feat: create default collection after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class LoginController extends Controller
 {
@@ -27,6 +29,14 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function authenticated(Request $request, $user)
+    {
+        Artisan::call('db:seed', [
+            '--class' => 'CollectionsTableSeeder',
+            '--force' => true
+        ]);
+    }
 
     /**
      * Create a new controller instance.
